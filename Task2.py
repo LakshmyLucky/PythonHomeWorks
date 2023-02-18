fc6171b6ed88454e7d3e1bb270fea02f92ba8339
LakshmyLucky <91207057+LakshmyLucky@users.noreply.github.com>
2023-02-18 22:51:45 +0300

    Приведение типа вынесена за пределы цикла

--- Task2.py
+++ Task2.py
@@ -1,11 +1,10 @@
 num = input("Введите трехзначное число: ")
-
 if len(num) != 3: 
     print("Число должно состоять из 3 цифр!")
 else:
     sum = 0
-    while int(num) > 0:
-        sum += int(num) % 10
-        num = int(num) // 10
-        
+    num = int(num)
+    while num > 0:
+        sum += num % 10
+        num = num // 10
     print("Сумма цифр заданного числа =", sum)
